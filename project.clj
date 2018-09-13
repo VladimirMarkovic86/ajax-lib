217fee160a623dd6ce2c0b87f87bd79c953b94e6
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-13 17:55:20 +0200

    Formatting code

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject org.vladimir/ajax-lib "0.1.0"
-  :description    "Simple AJAX library"
-  :url            "http://gitlab:1610/VladimirMarkovic86/ajax-lib"
-  :license        {:name "Eclipse Public License"
-                   :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies   [[org.clojure/clojure "1.9.0"]
-                   [org.clojure/clojurescript "1.10.339"]
-                   ]
+  :description "Simple AJAX library"
+  :url "http://gitlab:1610/VladimirMarkovic86/ajax-lib"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.339"]
+                 ]
   :source-paths ["src/cljc" "src/cljs"])
 
