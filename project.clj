84c2ae3ab0d9fcbb6cd353deeec452345dbe09fd
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:43:47 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject org.vladimir/ajax-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"
   :description "Simple AJAX library"
-  :url "http://gitlab:1610/VladimirMarkovic86/ajax-lib"
+  :url "https://github.com/VladimirMarkovic86/ajax-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  ]
+
+  :min-lein-version "2.0.0"
+    
   :source-paths ["src/cljc" "src/cljs"])
 
